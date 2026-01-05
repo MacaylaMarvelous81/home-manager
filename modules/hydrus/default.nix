ba9ae6dd1c8cf43cabc39e69db126eeb0ce12f01
Jomar Milan <jomarm@jomarm.com>
2026-01-04 21:22:47 -0800

    Move hydrus network wrapping into another derivation

--- modules/hydrus/default.nix
+++ modules/hydrus/default.nix
@@ -10,22 +10,34 @@ let
       tag = "v${ finalAttrs.version }";
       hash = "sha256-OH07OvN5EaEsjlUHUJMqproiVcN75yL9u7lnCjXSITo=";
     };
-
-    postInstall = ''
-      wrapProgram $out/bin/hydrus-client \
-        --add-flag '-d=${ config.home.homeDirectory }/Hydrus'
-    '';
   });
+  hydrus-wrapped = pkgs.symlinkJoin {
+    inherit (hydrus) pname version meta;
+    paths = [ hydrus ];
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/hydrus-client \
+        --unset WAYLAND_DISPLAY \
+        --add-flag '-d=${ config.home.homeDirectory }/${ cfg.dbDir }'
+    '';
+  };
 in {
   options.usermod.hydrus = {
     enable = lib.mkEnableOption "hydrus network configuration";
+    dbDir = lib.mkOption {
+      type = lib.types.str;
+      description = "Location of the hydrus network database directory relative to the home directory";
+      default = "Hydrus";
+    };
   };
 
   config = lib.mkIf cfg.enable {
-    home.packages = [ hydrus ];
+    home.packages = [ hydrus-wrapped ];
 
     home.file = {
-      "Hydrus/static/qss/user.qss".source = lib.mkIf config.stylix.enable (config.lib.stylix.colors {
+      # to apply this stylesheet, set the Qt stylesheet option under
+      # style in the client options
+      "${ cfg.dbDir }/static/qss/user.qss".source = lib.mkIf config.stylix.enable (config.lib.stylix.colors {
         template = ./user.qss.mustache;
         extension = ".qss";
       });
