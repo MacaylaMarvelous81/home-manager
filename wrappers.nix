84c63a1436aeda4b1c615c60d7fb4b1849d326cf
Jomar Milan <jomarm@jomarm.com>
2026-01-24 09:24:21 -0800

    Stop partially evaluating wrapperModules

--- wrappers.nix
+++ wrappers.nix
@@ -4,28 +4,34 @@
   wrappers ? (import (fetchTarball "https://github.com/BirdeeHub/nix-wrapper-modules/archive/9a9f10119294b22de06f62b38cf7c41f12adbdc6.tar.gz") { inherit pkgs; }),
 }:
 let
-  wrappedModules = import ./wrapperModules {
-    inherit lib;
-    wlib = wrappers.lib;
-  };
+  wrapperModules = import ./wrapperModules { inherit lib; };
 in rec {
-  atool = wrappers.wrappedModules.atool.wrap {
+  atool = wrappers.wrappers.atool.wrap {
     inherit pkgs;
     tools.paths = {
       rar = "${ pkgs.rar }/bin/rar";
       unrar = "${ pkgs.unrar }/bin/unrar";
     };
   };
-  neovide = wrappedModules.neovide.wrap {
-    inherit pkgs;
-    settings = {
-      neovim-bin = "${ neovim }/bin/nvim";
+  neovide = wrappers.lib.evalPackage ({ ... }: {
+    imports = [ wrapperModules.neovide ];
 
-      font = {
-        normal = "FiraCode Nerd Font";
-        size = 12;
+    config = {
+      inherit pkgs;
+
+      settings = {
+        neovim-bin = "${ neovim }/bin/nvim";
+
+        font = {
+          normal = "FiraCode Nerd Font";
+          size = 12;
+        };
       };
     };
-  };
-  neovim = wrappedModules.neovim.wrap { inherit pkgs; };
+  });
+  neovim = wrappers.lib.evalPackage ({ ... }: {
+    imports =  [ wrapperModules.neovim ];
+
+    config = { inherit pkgs; };
+  });
 }
