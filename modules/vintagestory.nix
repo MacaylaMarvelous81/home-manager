ccfb8d0a9c59ed900c57146bbbc16f6fb63bfc83
Jomar Milan <jomarm@jomarm.com>
2026-02-18 10:39:26 -0800

    Add Vintage Story module

---
+++ modules/vintagestory.nix
@@ -0,0 +1,22 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  cfg = config.usermod.vintagestory;
+in
+{
+  options.usermod.vintagestory = {
+    enable = lib.mkEnableOption "Vintage Story game";
+  };
+
+  config = lib.mkIf cfg.enable {
+    usermod.unfree.pkgnames = [ "vintagestory" ];
+
+    home.packages = with pkgs; [ vintagestory ];
+
+    home.sessionVariables.VINTAGE_STORY = "${pkgs.vintagestory}";
+  };
+}
