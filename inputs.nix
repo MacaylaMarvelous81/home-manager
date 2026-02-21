44e21ee74f8278e6b474252f3a5952108da1864d
Jomar Milan <jomarm@jomarm.com>
2026-02-20 16:55:20 -0800

    Add erosanix

--- inputs.nix
+++ inputs.nix
@@ -1,23 +1,28 @@
-{ sources }: { pkgs, lib, ... }:
+{ sources }:
+{ pkgs, lib, ... }:
 let
   niri-flake = (import sources.flake-compat) { src = sources.niri-flake; };
-in {
+in
+{
   _module.args =
     let
       wrappers = import sources.wrappers {
         inherit pkgs;
         nixpkgs = sources.nixpkgs;
       };
-    in {
+      erosanix = (import sources.flake-compat) { src = sources.erosanix; };
+    in
+    {
+      inherit erosanix;
       wrappers = import ./wrappers.nix { inherit pkgs lib wrappers; };
-      macaylamarvelous81-pkgs = import sources.macaylamarvelous81-pkgs { inherit pkgs lib; };
+      macaylamarvelous81-pkgs = import sources.macaylamarvelous81-pkgs { inherit pkgs lib erosanix; };
     };
 
   imports = [
     niri-flake.outputs.homeModules.niri
     niri-flake.outputs.homeModules.stylix
-    "${ sources.noctalia-shell }/nix/home-module.nix"
+    "${sources.noctalia-shell}/nix/home-module.nix"
     (import sources.stylix).homeModules.stylix
-    "${ sources.sops-nix }/modules/home-manager/sops.nix"
+    "${sources.sops-nix}/modules/home-manager/sops.nix"
   ];
 }
