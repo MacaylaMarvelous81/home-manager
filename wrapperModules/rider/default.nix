f7d5a72223e1507c63dd58f12d7a8da272bfca0a
Jomar Milan <jomarm@jomarm.com>
2026-03-07 16:51:15 -0800

    Remove JetBrains Rider wrapper

--- wrapperModules/rider/default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, lib, wlib, ... }:
-{
-  imports = [ wlib.modules.default ];
-
-  config = {
-    package = lib.mkDefault pkgs.jetbrains.rider;
-    extraPackages = with pkgs; [ dotnet-sdk_10 dotnet-runtime_10 dotnet-aspnetcore_10 ];
-  };
-}
--- wrappers.nix
+++ wrappers.nix
@@ -1,21 +1,21 @@
 {
-  pkgs ? import <nixpkgs> {},
+  pkgs ? import <nixpkgs> { },
   lib ? pkgs.lib,
-  wrappers ? (import (fetchTarball "https://github.com/BirdeeHub/nix-wrapper-modules/archive/9a9f10119294b22de06f62b38cf7c41f12adbdc6.tar.gz") { inherit pkgs; }),
+  wrappers ? (
+    import
+      (fetchTarball "https://github.com/BirdeeHub/nix-wrapper-modules/archive/9a9f10119294b22de06f62b38cf7c41f12adbdc6.tar.gz")
+      { inherit pkgs; }
+  ),
 }:
 let
   wrapperModules = import ./wrapperModules { inherit lib; };
-in {
+in
+{
   atool = wrappers.wrappers.atool.wrap {
     inherit pkgs;
     tools.paths = {
-      rar = "${ pkgs.rar }/bin/rar";
-      unrar = "${ pkgs.unrar }/bin/unrar";
+      rar = "${pkgs.rar}/bin/rar";
+      unrar = "${pkgs.unrar}/bin/unrar";
     };
   };
-  rider = wrappers.lib.evalPackage({ ... }: {
-    imports = [ wrapperModules.rider ];
-
-    config = { inherit pkgs; };
-  });
 }
