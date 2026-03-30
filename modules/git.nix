3cded704500974fe0d3a982ef23a61932ad95fe4
Jomar Milan <jomarm@jomarm.com>
2026-03-29 22:51:05 -0700

    Use programs.git.signing options

--- modules/git.nix
+++ modules/git.nix
@@ -1,7 +1,13 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 let
   cfg = config.usermod.git;
-in {
+in
+{
   options.usermod.git = {
     enable = lib.mkEnableOption "manage git config";
   };
@@ -10,24 +16,23 @@ in {
     programs.git = {
       enable = true;
       package = pkgs.gitFull;
+      signing = {
+        format = "openpgp";
+        key = "F954C5C95AE7A312183DA76C6AC46A6F9A5618D8";
+        signByDefault = true;
+      };
       settings = {
         init.defaultBranch = "master";
-        commit.gpgsign = true;
         user = {
           name = "Jomar Milan";
           email = "jomarm@jomarm.com";
-          signingkey = "F954C5C95AE7A312183DA76C6AC46A6F9A5618D8";
-        };
-        tag = {
-          gpgsign = true;
-          forcesignannotated = true;
         };
         sendemail = {
           smtpencryption = "ssl";
           smtpserver = "smtp.emailarray.com";
           smtpuser = "jomarm@jomarm.com";
         };
-        "diff \"json\"".textconv = "${ pkgs.jq }/bin/jq .";
+        "diff \"json\"".textconv = "${pkgs.jq}/bin/jq .";
       };
     };
   };
