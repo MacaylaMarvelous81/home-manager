84c0914363775ce58c4e9e5d6bdae58c9e00fd3f
Jomar Milan <jomarm@jomarm.com>
2026-04-25 20:34:43 -0700

    Add neovide

--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -5,12 +5,25 @@ in
 {
   options.usermod.nixvim = {
     enable = lib.mkEnableOption "nixvim module";
+    neovide = lib.mkEnableOption "neovide";
   };
 
   config = lib.mkIf cfg.enable {
     programs.nixvim = {
       enable = true;
       imports = [ ../nixvim/config ];
+    };
+
+    programs.neovide = lib.mkIf cfg.neovide {
+      enable = true;
+    };
+
+    programs.niri = lib.mkIf (cfg.neovide && config.usermod.niri.enable) {
+      settings = with config.lib.niri.actions; {
+        binds = {
+          "Mod+Z".action = spawn "${config.programs.neovide.package}/bin/neovide";
+        };
+      };
     };
   };
 }
