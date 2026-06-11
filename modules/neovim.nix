bfff01705fb0ac27ca668ac1b6bfd90bfa9f6d4d
Jomar Milan <jomarm@jomarm.com>
2026-06-10 18:27:47 -0700

    Configure live-share.nvim

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -31,6 +31,37 @@ in
           '';
         };
       };
+      plugins = {
+        live-share = ''
+          return {
+            "azratul/live-share.nvim",
+            config = function()
+              require("live-share.provider").register("loopback", {
+                command = function(_, port, service_url)
+                  return string.format(
+                    [[printf 'tcp://127.0.0.1:%d\n' > %s; sleep infinity]],
+                    port, service_url)
+                end,
+                pattern = "tcp://[%w._-]+:%d+",
+              })
+              require("live-share.provider").register("raspberrypi", {
+                command = function(cfg, port_internal, service_url)
+                  return string.format(
+                    "ssh -R 0.0.0.0:%d:localhost:%d raspberrypi " .. "'echo tcp://24.130.29.83:%d; sleep infinity' > %s 2>/dev/null",
+                    cfg.port, port_internal, cfg.port, service_url)
+                end,
+                pattern = "tcp://[%w.+=]+:%d+",
+              })
+              require("live-share").setup({
+                username = "jomarm",
+                service = "raspberrypi",
+                port = 8443,
+                openssl_lib = "${pkgs.openssl.out}/lib/libcrypto.so.3",
+              })
+            end,
+          }
+        '';
+      };
       extraPackages = with pkgs; [
         nixd
         nixfmt
