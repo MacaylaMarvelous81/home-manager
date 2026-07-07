d21e419c174685cb7220c5d505046fb4bced4d41
Jomar Milan <jomarm@pyrodax.com>
2026-07-06 21:36:02 -0700

    Configure rofi styles

--- modules/rofi.nix
+++ modules/rofi.nix
@@ -13,9 +13,35 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    programs.rofi.enable = true;
+    programs.rofi = {
+      enable = true;
+      theme =
+        let
+          inherit (config.lib.formats.rasi) mkLiteral;
+        in
+        {
+          window = {
+            border = 2;
+            border-radius = 12;
+          };
+          listview = {
+            border-radius = 10;
+            border = 2;
+            padding = 20;
+            margin = mkLiteral "20px 30px 30px 30px";
+            spacing = mkLiteral "0.3em";
+          };
+          element = {
+            spacing = mkLiteral "0.5em";
+            children = map mkLiteral [
+              "element-icon"
+              "element-text"
+            ];
+          };
+        };
+    };
 
-    programs.niri = lib.mkIf config.usermod.rofi.enable {
+    programs.niri = lib.mkIf config.programs.niri.enable {
       settings = with config.lib.niri.actions; {
         binds = {
           "Mod+Space".action = spawn "${config.programs.rofi.package}/bin/rofi" "-show" "drun";
